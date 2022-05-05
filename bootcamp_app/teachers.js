d32abba1c9e730bb0ea4667ff355bd9e99875eb0
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-04 21:22:17 -0500

    Refactor code to use parameterized query to avoid SQL injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -17,9 +17,10 @@ pool
     JOIN assistance_requests ON teacher_id = teachers.id
     JOIN students ON student_id = students.id
     JOIN cohorts ON cohort_id = cohorts.id
-    WHERE cohorts.name LIKE '%${cohortName}%'
+    WHERE cohorts.name LIKE $1
     ORDER BY teacher;
-    `
+    `,
+    [`%${cohortName}%`]
   )
   .then((res) => {
     res.rows.forEach((row) => {
